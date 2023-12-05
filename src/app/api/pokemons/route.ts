6f60f8def353e78b42e2ae7c404bef4d705d7dbf
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-05 11:18:24 +0100

    feat: filter pokemons by name

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -2,7 +2,16 @@ import { db } from "@/lib/db"
 import { NextResponse, type NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
-  const data = await db.pokemon.findMany()
+  const params = request.nextUrl.searchParams
+
+  const data = await db.pokemon.findMany({
+    where: {
+      name: {
+        contains: params.get('filter[name]') ?? undefined,
+        mode: 'insensitive',
+      }
+    },
+  })
 
   return NextResponse.json(
     { data },
