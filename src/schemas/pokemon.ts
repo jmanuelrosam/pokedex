f39011d1b3dd7357ab28ef9824981ad08e102bfa
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-05 16:24:18 +0100

    feat: add schema for pokemons

---
+++ src/schemas/pokemon.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const pokemonSchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  image: z.string().url().optional(),
+  height: z.number().int().positive(),
+  weight: z.number().int().positive(),
+  stats: z.array(
+    z.object({
+      name: z.string(),
+      value: z.number().int().positive(),
+    })
+  ),
+  category: z.string(),
+  abilities: z.array(z.string()),
+})
