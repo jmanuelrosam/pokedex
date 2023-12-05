462eee14fab76c653f9cb7ede2f6dcbc9e7a527c
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-05 11:42:01 +0100

    fesat: check sortable fields

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -1,11 +1,15 @@
 import { db } from "@/lib/db"
 import { NextResponse, type NextRequest } from "next/server"
 
+const SORTABLE_FIELDS:readonly string[] = ['name', 'height', 'weight']
+
 export async function GET(request: NextRequest) {
   const params = request.nextUrl.searchParams
 
   const sort = params.get('sort') ?? 'name'
-  const normalizeSort = sort.split(',').map((field) => {
+  const normalizeSort = sort.split(',')
+    .filter((field) => SORTABLE_FIELDS.includes(field))
+    .map((field) => {
     if (field.startsWith('-')) {
       return { [field.slice(1)]: 'desc' }
     }
